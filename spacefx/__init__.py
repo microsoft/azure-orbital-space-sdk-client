3f6237ec6cd2258795a2a134af30a0a02affb53a
Ryan Campbell <bigtallcampbell@gmail.com>
2024-07-09 17:49:54 +0000

    updating for logger and new proto paths

---
+++ spacefx/__init__.py
@@ -0,0 +1,9 @@
+from spacefx import protos
+from spacefx import _sdk_client
+from spacefx import client
+from spacefx import logging
+from spacefx import position
+from spacefx import link
+from spacefx import sensor
+
+logger = logging.__SpaceFxLogger
