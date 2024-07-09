f0fb4dd91c656c89119953d04a9dc79c06abaf96
Ryan Campbell <bigtallcampbell@gmail.com>
2024-07-09 20:23:02 +0000

    updating to enable python wheel build

--- spacefx/_sdk_client.py
+++ spacefx/_sdk_client.py
@@ -1,6 +1,25 @@
 import os
-
+from pathlib import Path
 import pythonnet
+
+
+def find_git_root(starting_directory):
+    """
+    Search for a .git directory by moving up the directory tree from the starting directory.
+    If found, return the path to the directory containing the .git directory.
+    If not found and the root of the directory tree is reached, return None.
+    """
+    current_directory = starting_directory
+    while True:
+        # Check if .git directory exists in the current directory
+        if os.path.isdir(os.path.join(current_directory, ".git")):
+            return os.path.join(current_directory, ".git")
+        # Move up one directory level
+        parent_directory = os.path.dirname(current_directory)
+        if parent_directory == current_directory:
+            # Root of the directory tree is reached without finding .git
+            return None
+        current_directory = parent_directory
 
 
 def search_file(filename, search_path):
@@ -11,42 +30,54 @@ def search_file(filename, search_path):
                 return os.path.join(dirpath, file)
     raise FileNotFoundError(f"{filename} not found in {search_path}")
 
+# Find the dotnet directory
+PROD_DOTNET_DIR = "/usr/share/dotnet/shared"
+DOTNET_DIR=""
 
-prod_dotnet_dir = "/usr/share/dotnet/shared"
-dev_dotnet_dir = "/root/.dotnet/shared"
-dotnet_dir = ""
+# SpaceFX-Dev installed dotnet to the .git directory so it can be used by the
+# debugShim and the devcontainer.  This will locate the .git directory by
+# walking up the tree
+DEV_DOTNET_DIR = find_git_root(os.path.dirname(os.path.abspath(__file__)))
 
-if os.path.exists(prod_dotnet_dir):
-    dotnet_dir = prod_dotnet_dir
-elif os.path.exists(dev_dotnet_dir):
-    dotnet_dir = dev_dotnet_dir
-else:
-    raise ValueError(f"Was not able to find dotnet directory as '{prod_dotnet_dir}' nor '{dev_dotnet_dir}'.  Please check that dotnet is installed")
+if DEV_DOTNET_DIR:
+    DEV_DOTNET_DIR = os.path.join(DEV_DOTNET_DIR, "spacefx-dev", "dotnet", "shared")
+    DOTNET_DIR = DEV_DOTNET_DIR if os.path.exists(DEV_DOTNET_DIR) else None
+
+if not DOTNET_DIR:
+    DOTNET_DIR = PROD_DOTNET_DIR if os.path.exists(PROD_DOTNET_DIR) else None
+
+if not DOTNET_DIR:
+    raise ValueError(f"Was not able to find dotnet directory as '{PROD_DOTNET_DIR}' nor within a '{DEV_DOTNET_DIR}'. Please check that dotnet is installed")
+
 
 # Recursively search and load the runtimeconfig.json - this allows for dotnet minor version changes
-runtimeconfig_file = search_file("Microsoft.AspNetCore.App.runtimeconfig.json", dotnet_dir)
+runtimeconfig_file = search_file("Microsoft.AspNetCore.App.runtimeconfig.json", DOTNET_DIR)
 
 pythonnet.load("coreclr", runtime_config=runtimeconfig_file)
 import clr
 from System.Reflection import Assembly
 
 # Load the dotnet dlls by walking down the tree
-for dirpath, dirnames, filenames in os.walk(os.path.join(dotnet_dir, 'Microsoft.AspNetCore.App')):
+for dirpath, dirnames, filenames in os.walk(os.path.join(DOTNET_DIR, 'Microsoft.AspNetCore.App')):
     if "runtimes" in dirpath:
         continue
     for file in filenames:
         if file.endswith(".dll"):
             Assembly.LoadFile(os.path.join(dirpath, file))
 
+
 # Load the client adapter library which is in the parent directory / spacefxClient
-for dirpath, dirnames, filenames in os.walk(os.path.join(os.path.dirname(__file__), 'spacefxClient')):
-    if "runtimes" in dirpath:
-        continue
-    for file in filenames:
-        if file.endswith(".dll"):
-            dll_lib = Assembly.LoadFile(os.path.join(dirpath, file))
-            if file == "sdk-dotnet.dll":
-                assembly = clr.AddReference(os.path.join(dirpath, file))
+base_dir = Path(__file__).parent / 'spacefxClient'
+
+# Find "spacesdk-client.dll" in any subdirectory
+spacesdk_client_dll = next(base_dir.rglob('spacesdk-client.dll'), None)
+
+# Found it.  Add it
+if spacesdk_client_dll:
+    assembly = clr.AddReference(str(spacesdk_client_dll))
+else:
+    raise ValueError(f"The DLL 'spacesdk-client.dll' was not found in '{str(base_dir)}'. Please check that the client library was built and deployed to '{str(base_dir)}'")
+
 
 # Import the .NET SpaceFx namespace
 import Microsoft.Azure.SpaceFx
